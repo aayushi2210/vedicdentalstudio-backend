83f9cf84f54323a0da2f111e35df230659600196
Aayushi Anand <anandaayushi2210@gmail.com>
2026-06-26 12:54:59 +0530

    Add files via upload

---
+++ models/index.js
@@ -0,0 +1,14 @@
+// Single place to import every model:
+// const { Patient, Appointment, Invoice } = require('./models');
+module.exports = {
+  Clinic:          require('./Clinic'),
+  Patient:         require('./Patient'),
+  Appointment:     require('./Appointment'),
+  PackageTemplate: require('./PackageTemplate'),
+  Package:         require('./Package'),
+  TreatmentPlan:   require('./TreatmentPlan'),
+  Session:         require('./Session'),
+  Invoice:         require('./Invoice'),
+  Feedback:        require('./Feedback'),
+  Document:        require('./Document'),   // NEW
+};
